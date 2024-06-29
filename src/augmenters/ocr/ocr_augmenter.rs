8fb1d4449b2c7f5215c7822b827c7337d0eaff48
Simon Théorêt <simonteoret@hotmail.com>
2024-06-29 16:05:20 -0400

    corrected name of field

--- src/augmenters/ocr/ocr_augmenter.rs
+++ src/augmenters/ocr/ocr_augmenter.rs
@@ -6,7 +6,11 @@ struct OCRAugmenter {
     content: Vec<String>,
     modified_content: Vec<String>,
     // List of augmentation to apply
-    kb_augmentation_map: OCRAugmentationMap,
+    ocr_augmentation_map: OCRAugmentationMap,
     // Probability of applying an augmentation on a word
     augmentation_proba: f32,
+}
+
+impl Augment for OCRAugmenter{
+    fn augment()
 }
